d5f57cdb900bf7cec62670783837bebe65ceaa16
Reese Sorrell <joreesesorrell@gmail.com>
2022-06-02 17:48:09 -0500

    Checks for win condition and breaks buttons after

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay() {
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0) {
@@ -20,33 +23,66 @@ function playRound(playerSelection, computerSelection) {
     }
     if (lowerPlayerSelection === 'rock') {
         if (computerSelection === 'paper') {
+            computerScore += 1;
             return computerWinMessage;
         }
         if (computerSelection === 'scissors') {
+            playerScore += 1;
             return playerWinMessage;
         }
     }
     if (lowerPlayerSelection === 'paper') {
         if (computerSelection === 'scissors') {
+            computerScore += 1;
             return computerWinMessage;
         }
         if (computerSelection === 'rock') {
+            playerScore += 1;
             return playerWinMessage;
         }
     }
     if (lowerPlayerSelection === 'scissors') {
         if (computerSelection === 'rock') {
+            computerScore += 1;
             return computerWinMessage;
         }
         if (computerSelection === 'paper') {
+            playerScore += 1;
             return playerWinMessage;
         }
     }
 }
 
+function breakButtons() {
+    buttons.forEach(element => {
+        element.removeEventListener('click', readChoice);
+    });
+}
+
+function checkWinner() {
+    const results = document.querySelector('.results');
+    if (computerScore === 5) {
+        results.textContent = 'gg, computer is just better.';
+        breakButtons();
+    }
+    else if (playerScore === 5) {
+        results.textContent = 'good job, you beat a dumb computer.'
+        breakButtons();
+    }
+}
+
+function updateScore() {
+    const playerScoreText = document.querySelector('.player-score');
+    playerScoreText.textContent = `Player: ${playerScore}`;
+    const computerScoreText = document.querySelector('.computer-score');
+    computerScoreText.textContent = `Computer: ${computerScore}`;
+}
+
 function displayResults(message) {
     const results = document.querySelector('.results');
     results.textContent = message;
+    updateScore();
+    checkWinner();
 }
 
 function readChoice() {
@@ -60,3 +96,5 @@ const buttons = Array.from(document.querySelectorAll('.choice'));
 buttons.forEach(element => {
     element.addEventListener('click', readChoice);
 }); // adds listener to every button
+
+updateScore();
